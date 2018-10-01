5856f82866bcfe241ba269dda103f4b08d9ac786
x230 <chase.langford@gmail.com>
2018-09-30 21:33:46 -0400

    Lab 3 finished

--- app/src/main/java/com/example/x230/androidlabs/StartActivity.java
+++ app/src/main/java/com/example/x230/androidlabs/StartActivity.java
@@ -26,6 +26,7 @@ public class StartActivity extends Activity {
         });
 
     }
+    @Override
     public void onActivityResult(int requestCode, int responseCode, Intent data)
     {
         if((responseCode == Activity.RESULT_OK)&&(requestCode == 50)){
@@ -38,24 +39,28 @@ public class StartActivity extends Activity {
             toast.show();
         }
     }
-
+    @Override
     protected void onResume(){
         Log.i(ACTIVITY_NAME, "In onResume");
         super.onResume();
 
     }
+    @Override
     protected void onStart(){
         Log.i(ACTIVITY_NAME, "In onStart");
         super.onStart();
     }
+    @Override
     protected void onPause(){
         Log.i(ACTIVITY_NAME, "In onPause");
         super.onPause();
     }
+    @Override
     protected void onStop(){
         Log.i(ACTIVITY_NAME, "In onStop");
         super.onStop();
     }
+    @Override
     protected void onDestroy(){
         Log.i(ACTIVITY_NAME, "In onDestroy");
         super.onDestroy();
